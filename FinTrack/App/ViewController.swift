c6eba81c982eabfc3d47a3d58992531dd7bbd8bf
Diggo Silva <rdiggosilva@gmail.com>
2025-12-02 11:09:50 -0300

    Initial configuration

---
+++ FinTrack/App/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FinTrack
+//
+//  Created by Diggo Silva on 02/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+}
