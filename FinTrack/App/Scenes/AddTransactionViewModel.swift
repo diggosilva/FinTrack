245a996c376bb5eb2e8b3f9af6dc015eec8e2e89
Diggo Silva <rdiggosilva@gmail.com>
2025-12-05 09:19:12 -0300

    AddTransactionViewModel

--- FinTrack/App/Scenes/AddTransactionViewModel.swift
+++ FinTrack/App/Scenes/AddTransactionViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+protocol AddTransactionViewModelDelegate: AnyObject {
+    func errorOccured(_ message: String)
+    func savedTransaction()
+}
+
+protocol AddTransactionViewModelProtocol {
+    func save(date: Date, incomeText: String?, expenseText: String?)
+}
+
+class AddTransactionViewModel: AddTransactionViewModelProtocol {
+    
+    weak var delegate: AddTransactionViewModelDelegate?
+    
+    func save(date: Date, incomeText: String?, expenseText: String?) {
+        guard let incomeText = incomeText, !incomeText.isEmpty,
+              let expenseText = expenseText, !expenseText.isEmpty else {
+            delegate?.errorOccured("Preencha todos os campos.")
+            return
+        }
+        
+        guard let income = Double(incomeText),
+              let expense = Double(expenseText) else {
+            delegate?.errorOccured("Valores inválidos.")
+            return
+        }
+        
+        delegate?.savedTransaction()
+    }
+}
