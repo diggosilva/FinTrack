8ffaac8283ef1d60141093c58bac371a4e4c1ebe
Diggo Silva <rdiggosilva@gmail.com>
2025-12-06 08:02:57 -0300

    Refactored SceneDelegate to receive AddTransactionViewModel

--- FinTrack/Resources/SceneDelegate.swift
+++ FinTrack/Resources/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: AddTransactionViewController())
+        let viewModel = AddTransactionViewModel()
+        window.rootViewController = UINavigationController(rootViewController: AddTransactionViewController(viewModel: viewModel))
         window.makeKeyAndVisible()
         self.window = window
     }
