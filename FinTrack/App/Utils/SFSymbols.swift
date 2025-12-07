0b30e85686b3abf1ef850cd11db887382106ecde
Diggo Silva <rdiggosilva@gmail.com>
2025-12-07 08:49:53 -0300

    Created new icons for SFSymbols file

--- FinTrack/App/Utils/SFSymbols.swift
+++ FinTrack/App/Utils/SFSymbols.swift
@@ -5,4 +5,13 @@
 //  Created by Diggo Silva on 06/12/25.
 //
 
-import Foundation
+import UIKit
+
+class SFSymbols {
+    static let upArrow = UIImage(systemName: "arrowshape.up.fill")?.withTintColor(.systemGreen, renderingMode: .alwaysOriginal)
+    static let downArrow = UIImage(systemName: "arrowshape.down.fill")?.withTintColor(.systemRed, renderingMode: .alwaysOriginal)
+    static let plusCircle = UIImage(systemName: "plus.circle")
+    static let plusCircleFill = UIImage(systemName: "plus.circle.fill")
+    static let chartBar = UIImage(systemName: "chart.bar")
+    static let chartBarFill = UIImage(systemName: "chart.bar.fill")
+}
