3a30643fadc38cf4948054de7e7edd36169b6f13
Diggo Silva <rdiggosilva@gmail.com>
2025-12-07 08:50:32 -0300

    Created methodos to format date and currency

--- FinTrack/App/Utils/Helpers.swift
+++ FinTrack/App/Utils/Helpers.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+// MARK: Extensions
+
 extension UIViewController {
     func presentDSAlert(title: String, message: String, handler: ((UIAlertAction) -> Void)? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
@@ -14,4 +16,21 @@ extension UIViewController {
         alert.addAction(ok)
         present(alert, animated: true)
     }
+}
+
+// MARK: Methods
+
+func formatDateStyle(date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    return formatter.string(from: date)
+}
+
+func formatCurrency(_ value: Double) -> String {
+    let formatter = NumberFormatter()
+    formatter.locale = Locale(identifier: "pt_BR")
+    formatter.numberStyle = .currency
+    formatter.currencyCode = "BRL"
+    guard let formattedString = formatter.string(from: NSNumber(value: value)) else { return "" }
+    return formattedString
 }
