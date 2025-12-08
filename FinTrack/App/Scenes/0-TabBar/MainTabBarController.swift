ad75edfc725aabf772413fc93ade5b355e793664
Diggo Silva <rdiggosilva@gmail.com>
2025-12-08 08:18:39 -0300

    Created TabBar

--- FinTrack/App/Scenes/0-TabBar/MainTabBarController.swift
+++ FinTrack/App/Scenes/0-TabBar/MainTabBarController.swift
@@ -5,4 +5,22 @@
 //  Created by Diggo Silva on 07/12/25.
 //
 
-import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    let addTransactionVC = UINavigationController(rootViewController: AddTransactionViewController())
+    let transactionListVC = UINavigationController(rootViewController: TransactionListViewController())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTabBar()
+    }
+    
+    private func configTabBar() {
+        addTransactionVC.tabBarItem = UITabBarItem(title: "Registrar", image: SFSymbols.plusCircle, selectedImage: SFSymbols.plusCircleFill)
+        transactionListVC.tabBarItem = UITabBarItem(title: "Resumo", image: SFSymbols.chartBar, selectedImage: SFSymbols.chartBarFill)
+        
+        viewControllers = [addTransactionVC, transactionListVC]
+    }
+}
