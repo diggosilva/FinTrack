7925bc1fe9c90b47c026ec4b2a39ed002587c4e1
Diggo Silva <rdiggosilva@gmail.com>
2025-12-09 10:35:07 -0300

    Refactored to group, sort, by month

--- FinTrack/App/Scenes/2-TransactionList/TransactionListViewModel.swift
+++ FinTrack/App/Scenes/2-TransactionList/TransactionListViewModel.swift
@@ -7,34 +7,71 @@
 
 import Foundation
 
+struct SectionMonth {
+    let title: String
+    let items: [TransactionModel]
+}
+
 protocol TransactionListViewModelProtocol: AnyObject {
     var transactions: [TransactionModel] { get }
     func loadTransactions()
-    func numberOfRows() -> Int
-    func transactionForRow(at index: Int) -> TransactionModel
+    func numberOfSections() -> Int
+    func numberOfRows(in section: Int) -> Int
+    func transactionForRow(in section: Int, at index: Int) -> TransactionModel
+    func titleForSection(at index: Int) -> String
 }
 
 final class TransactionListViewModel: TransactionListViewModelProtocol {
     
+    private var sections: [SectionMonth] = []
     private(set) var transactions: [TransactionModel] = []
+    private let repository = TransactionRepository()
+    
+    init() {
+        loadTransactions()
+    }
     
     func loadTransactions() {
-        #warning("Implement the data fetching here")
-        
-        //Mock apenas pra ver se a lista está funcionando
-        transactions = [
-            TransactionModel(date: Date(), income: 100.0, expense: 0.0),
-            TransactionModel(date: Date().addingTimeInterval(-86_400), income: 50.0, expense: 0.0),
-            TransactionModel(date: Date().addingTimeInterval(-172_800), income: 0.0, expense: 25.0),
-            TransactionModel(date: Date().addingTimeInterval(-1_728_000), income: 0.0, expense: 30.0),
-        ]
+        transactions = loadAndSort()
+        let grouped = groupByMonth(transactions)
+        let sortedKeys = sortMonthKeys(grouped.keys)
+        sections = buildSections(grouped: grouped, sortedKeys: sortedKeys)
     }
     
-    func numberOfRows() -> Int {
-        transactions.count
+    private func loadAndSort() -> [TransactionModel] {
+        return repository.load().sorted(by: { $0.date > $1.date })
     }
     
-    func transactionForRow(at index: Int) -> TransactionModel {
-        return transactions[index]
+    private func groupByMonth(_ transactions: [TransactionModel]) -> [String : [TransactionModel]] {
+        return Dictionary(grouping: transactions) { $0.date.toMonthAndYear() }
+    }
+    
+    private func sortMonthKeys(_ keys: Dictionary<String, [TransactionModel]>.Keys) -> [String] {
+        return keys.sorted { $0.toDateFromMonthAndYear() > $1.toDateFromMonthAndYear() }
+    }
+    
+    private func buildSections(grouped: [String : [TransactionModel]], sortedKeys: [String]) -> [SectionMonth] {
+        return sortedKeys.map { key in
+            SectionMonth(
+                title: key,
+                items: (grouped[key]!.sorted(by: { $0.date > $1.date }))
+            )
+        }
+    }
+    
+    func numberOfSections() -> Int {
+        return sections.count
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        return sections[section].items.count
+    }
+    
+    func transactionForRow(in section: Int, at index: Int) -> TransactionModel {
+        return sections[section].items[index]
+    }
+    
+    func titleForSection(at index: Int) -> String {
+        return sections[index].title
     }
 }
