4a431188c940d7537e919a0379f50fa54c8daa98
Diggo Silva <rdiggosilva@gmail.com>
2025-12-15 07:38:25 -0300

    Created MockRepository

--- FinTrackTests/MockRepository.swift
+++ FinTrackTests/MockRepository.swift
@@ -5,4 +5,24 @@
 //  Created by Diggo Silva on 14/12/25.
 //
 
-import Foundation
+@testable import FinTrack
+
+class MockRepository: TransactionRepositoryProtocol {
+    
+    var isSuccess: Bool = true
+    var savedTransactions: [TransactionModel] = []
+    var saveCalled: Bool = false
+    
+    func save(_ transactions: [TransactionModel]) {
+        if isSuccess {
+            savedTransactions = transactions
+        } else {
+            savedTransactions = []
+        }
+        saveCalled = true
+    }
+    
+    func load() -> [TransactionModel] {
+        return savedTransactions
+    }
+}
