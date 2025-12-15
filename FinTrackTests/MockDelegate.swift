c49bb7ed637092b150c07105407f621be1383888
Diggo Silva <rdiggosilva@gmail.com>
2025-12-15 07:38:38 -0300

    Created MockDelegate

--- FinTrackTests/MockDelegate.swift
+++ FinTrackTests/MockDelegate.swift
@@ -5,4 +5,17 @@
 //  Created by Diggo Silva on 14/12/25.
 //
 
-import Foundation
+@testable import FinTrack
+
+final class MockDelegate: AddTransactionViewModelDelegate {
+    var lastErrorMessage: String?
+    var savedCalled = false
+
+    func errorOccured(_ message: String) {
+        lastErrorMessage = message
+    }
+
+    func savedTransaction() {
+        savedCalled = true
+    }
+}
