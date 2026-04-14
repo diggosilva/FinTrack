82177dd036aa34f0ea97f087973f5197d67aaf4e
Diggo Silva <rdiggosilva@gmail.com>
2026-04-14 12:28:29 -0300

    feat: Created KeyboardAwareViewController
    feat: Created UIView+FirstResponder

--- FinTrack/App/Core/KeyboardAwareViewController.swift
+++ FinTrack/App/Core/KeyboardAwareViewController.swift
@@ -5,4 +5,88 @@
 //  Created by Diggo Silva on 16/01/26.
 //
 
-import Foundation
+import UIKit
+
+class KeyboardAwareViewController: UIViewController {
+    
+    var keyboardScrollView: UIScrollView? {
+        nil
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupKeyboardObservers()
+        setupFieldFocusObservers()
+        setupDismissKeyboardGesture()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
+
+extension KeyboardAwareViewController {
+    
+    private func setupKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard
+            let scrollView = keyboardScrollView,
+            let frame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
+        else { return }
+        
+        let keyboardHeight = frame.height
+        
+        scrollView.contentInset.bottom = keyboardHeight + 16
+        scrollView.verticalScrollIndicatorInsets.bottom = keyboardHeight
+        
+        DispatchQueue.main.async {
+            if let activeField = self.view.findFirstResponder() {
+                let rect = activeField.convert(activeField.bounds, to: scrollView)
+                scrollView.scrollRectToVisible(rect, animated: true)
+            }
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        guard let scrollView = keyboardScrollView else { return }
+        scrollView.contentInset = .zero
+        scrollView.verticalScrollIndicatorInsets = .zero
+    }
+}
+
+extension KeyboardAwareViewController {
+    
+    private func setupFieldFocusObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(fieldDidBeginEditing), name: UITextField.textDidBeginEditingNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(fieldDidBeginEditing), name: UITextView.textDidBeginEditingNotification, object: nil)
+    }
+    
+    @objc private func fieldDidBeginEditing(_ notification: Notification) {
+        guard
+            let scrollView = keyboardScrollView,
+            let field = notification.object as? UIView
+        else { return }
+        
+        DispatchQueue.main.async {
+            let rect = field.convert(field.bounds, to: scrollView)
+            scrollView.scrollRectToVisible(rect, animated: true)
+        }
+    }
+}
+
+extension KeyboardAwareViewController {
+    
+    private func setupDismissKeyboardGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
--- FinTrack/App/Core/UIView+FirstResponder.swift
+++ FinTrack/App/Core/UIView+FirstResponder.swift
@@ -5,4 +5,17 @@
 //  Created by Diggo Silva on 16/01/26.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func findFirstResponder() -> UIView? {
+        if isFirstResponder { return self }
+        
+        for subview in subviews {
+            if let responder = subview.findFirstResponder() {
+                return responder
+            }
+        }
+        return nil
+    }
+}
